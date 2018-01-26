c57cb3342d44ae7f26847a7cff73542ab11059fd
Lady Cans <frc2881@gmail.com>
2018-01-25 19:09:57 -0600

    Programed/tweaked RumbleJoysticks command (the best command) Remove the deadband code fix drive in high gear so I doesn't go backwards

--- src/org/usfirst/frc2881/karlk/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc2881/karlk/subsystems/DriveSubsystem.java
@@ -17,9 +17,9 @@ import org.usfirst.frc2881.karlk.commands.DriveWithController;
  */
 public class DriveSubsystem extends Subsystem implements SendableWithChildren {
 
-    private static final double DEADBAND = 0.1;
 
-    //grab hardware objects from RobotMap and add them into the LiveWindow at the same ti
+
+    //grab hardware objects from RobotMap and add them into the LiveWindow at the same time
     //by making a call to the SendableWithChildren method add.
     private final SpeedController leftRearMotor = add(RobotMap.driveSubsystemLeftRearMotor);
     private final SpeedController leftFrontMotor = add(RobotMap.driveSubsystemLeftFrontMotor);
@@ -50,12 +50,10 @@ public class DriveSubsystem extends Subsystem implements SendableWithChildren {
 
     public void tankDrive(double leftSpeed, double rightSpeed) {
         // Use 'squaredInputs' to get better control at low speed
-        driveTrain.tankDrive(adjust(leftSpeed), adjust(rightSpeed), true);
+        driveTrain.tankDrive(leftSpeed,rightSpeed, true);
     }
 
-    private double adjust(double speed) {
-        return Math.abs(speed) <= DEADBAND ? 0.0 : speed;
-    }
+
 
     public void highGear() {
         gearShift.set(true);
