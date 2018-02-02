f97bdcfbb296ccc0f26a61d7686c43f87ba53c6e
Lady Cans <frc2881@gmail.com>
2018-02-01 21:22:05 -0600

    Added some encoder stuff to limit the movement of the lift as it goes to max and min height. STUDENT: Quinn MENTOR: Shawn

--- src/org/usfirst/frc2881/karlk/commands/ControlArm.java
+++ src/org/usfirst/frc2881/karlk/commands/ControlArm.java
@@ -3,6 +3,7 @@ package org.usfirst.frc2881.karlk.commands;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc2881.karlk.Robot;
+import org.usfirst.frc2881.karlk.RobotMap;
 
 /**
  * This command runs the arm.
@@ -21,10 +22,34 @@ public class ControlArm extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        double speed = Robot.oi.manipulator.getY(GenericHID.Hand.kRight);
+        double speed = -Robot.oi.manipulator.getY(GenericHID.Hand.kRight);
+        double distance = RobotMap.liftSubsystemArmEncoder.getDistance();
+        double min = -1;
+        double max = 1;
+        if (distance <= 0) {
+            min = 0;
+            max = 1;
+        } else if (distance >= 7) {
+            min = -1;
+            max = 0;
+        } else if (distance <= 2) {
+            min = distance * (-.4) - .2;
+        } else if (distance >= 5) {
+            max = distance * (-.4) + 3;
+        } else {
+            min = -1;
+            max = 1;
+        }
+        if (speed < min) {
+            speed = min;
+        }
+        if (speed > max) {
+            speed = max;
+        }
         Robot.liftSubsystem.armControl(speed);
-        // TODO maybe set limits for this later??
     }
+    // TODO maybe set limits for this later??
+
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
@@ -39,3 +64,4 @@ public class ControlArm extends Command {
 
 
 }
+
