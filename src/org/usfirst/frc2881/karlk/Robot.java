a1657a4a86ff14c74de90752bae1dadf44d173a7
LadyCans <frc2881@gmail.com>
2018-02-09 19:44:34 -0600

    added boolean to robot.java

--- src/org/usfirst/frc2881/karlk/Robot.java
+++ src/org/usfirst/frc2881/karlk/Robot.java
@@ -25,9 +25,6 @@ import org.usfirst.frc2881.karlk.subsystems.PrettyLightsSubsystem;
  */
 public class Robot extends TimedRobot {
 
-    Command autonomousCommand;
-    SendableChooser<Command> chooser = new SendableChooser<>();
-
     public static OI oi;
     public static DriveSubsystem driveSubsystem;
     public static IntakeSubsystem intakeSubsystem;
@@ -35,6 +32,10 @@ public class Robot extends TimedRobot {
     public static ClimbingSubsystem climbingSubsystem;
     public static CompressorSubsystem compressorSubsystem;
     public static PrettyLightsSubsystem lightsSubsystem;
+
+    private Command autonomousCommand;
+    private SendableChooser<Command> chooser = new SendableChooser<>();
+    private boolean resetRobot;
 
     /**
      * This function is run when the robot is first started up and should be
@@ -75,6 +76,17 @@ public class Robot extends TimedRobot {
         SmartDashboard.putData("Auto mode", chooser);//make sure to add to SmartDashboard
     }
 
+
+    public boolean resetRobot() {
+        if (resetRobot) {
+            driveSubsystem.reset();
+            liftSubsystem.reset();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /**
      * This function is called when the disabled button is hit.
      * You can use it to reset subsystems before shutting down.
@@ -90,14 +102,15 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        driveSubsystem.reset();
-        liftSubsystem.reset();
+        resetRobot();
 
         autonomousCommand = chooser.getSelected();
         // schedule the autonomous command (example)
         if (autonomousCommand != null) {
             autonomousCommand.start();
         }
+
+
     }
 
     /**
