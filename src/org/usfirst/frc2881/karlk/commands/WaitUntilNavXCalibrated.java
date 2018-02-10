b93b21360842f3eaedc752d8763211998d5d7c93
Lady Cans <frc2881@gmail.com>
2018-02-10 13:07:11 -0600

    tweak waituntilnavxcalibrated
                             student emma mentor shawn

--- src/org/usfirst/frc2881/karlk/commands/WaitUntilNavXCalibrated.java
+++ src/org/usfirst/frc2881/karlk/commands/WaitUntilNavXCalibrated.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc2881.karlk.commands;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc2881.karlk.RobotMap;
 
-public class WaitUntilNavXCalibrated extends InstantCommand {
+public class WaitUntilNavXCalibrated extends Command {
     @Override
     protected boolean isFinished() {
         return !RobotMap.driveSubsystemNavX.isConnected() || !RobotMap.driveSubsystemNavX.isCalibrating();
