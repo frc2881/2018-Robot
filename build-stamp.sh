08a54e523f3ac5b32502c0228b0737fe232ef7d4
Shawn Smith <shawn@thena.net>
2018-02-11 16:12:11 -0600

    Fix build version logging for Windows

--- build-stamp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Print something that does a reasonable job of identifying the current version of the code
-echo $(date) - $(git name-rev --always HEAD)
-
-# Print the first line of the last commit message
-git log -1 --oneline --no-color
