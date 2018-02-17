b0a63ef7f2b7e8f8583ff3e961d5e965e5281496
ladycans <31555432+ladycans@users.noreply.github.com>
2018-02-16 21:34:49 -0600

    Eject testing (#64)
    * Experiment with ejecting the cube
    
    * Update: Eject Cube
    
    * Update: Eject Cube
    
    * Update: Eject Cube
    
    * Fix

--- src/org/usfirst/frc2881/karlk/commands/EjectCubeOnGround.java
+++ src/org/usfirst/frc2881/karlk/commands/EjectCubeOnGround.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc2881.karlk.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.PrintCommand;
+import edu.wpi.first.wpilibj.command.TimedCommand;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import edu.wpi.first.wpilibj.command.WaitForChildren;
 import org.usfirst.frc2881.karlk.Robot;
 import org.usfirst.frc2881.karlk.subsystems.IntakeSubsystem.GrasperState;
 import org.usfirst.frc2881.karlk.subsystems.LiftSubsystem;
@@ -15,21 +19,26 @@ public class EjectCubeOnGround extends CommandGroup {
 
     public EjectCubeOnGround() {
         super("EjectCubeOnGround");
-        requires(Robot.intakeSubsystem);
-        //final double ???????????= 0.45;
-        /*
-       1) Arm at 0 height
-       2) Claw releases
-       3) Grasper releases
-       4) Rollers 'eject'
-       6) etc.
-         */
-        addSequential(new LiftToHeight(LiftSubsystem.ZERO_ARM_HEIGHT));
-        addSequential(new SetClaw(ClawState.OPEN));
-        addSequential(new SetGrasper(GrasperState.OPEN));
-        addParallel(new SetRollers(Robot.intakeSubsystem.EJECT_SPEED));//This will set the motor to a slow speed backwards to eject the cube
+        //requires(Robot.intakeSubsystem);
+              /*
+      1) Open Grasper
+      2) Lift claw
+      2) Set rollers
+      3) Close Grasper
+      3) Open claw
 
+         */
+
+        addSequential(new PrintCommand("Eject Cube is working if you read this."));
+        addSequential(new SetGrasper(GrasperState.OPEN));
+        addSequential(new LiftToHeight(1.1)); //in feet
+        addSequential(new SetGrasper(GrasperState.CLOSED));
+        addSequential(new WaitCommand(0.3));
+        addParallel(new SetRollers(Robot.intakeSubsystem.EJECT_SPEED), 1.0);//This will set the motor to run backwards to eject the cube
+        addSequential(new SetClaw(ClawState.OPEN));
+        addSequential(new WaitForChildren());
     }
+
 
     // Called just before this Command runs the first time
     @Override
