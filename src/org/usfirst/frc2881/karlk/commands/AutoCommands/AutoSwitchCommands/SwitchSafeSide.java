3f50d7d855400e67de46b94b6b1d71ece79d4215
Lady Cans <frc2881@gmail.com>
2018-03-31 17:10:12 -0500

    good competition auto code i think
    STUDENTS: Tian
    MENTORS: Shawn

--- src/org/usfirst/frc2881/karlk/commands/AutoCommands/AutoSwitchCommands/SwitchSafeSide.java
+++ src/org/usfirst/frc2881/karlk/commands/AutoCommands/AutoSwitchCommands/SwitchSafeSide.java
@@ -3,10 +3,12 @@ package org.usfirst.frc2881.karlk.commands.AutoCommands.AutoSwitchCommands;
 import edu.wpi.first.wpilibj.command.ConditionalCommand;
 import org.usfirst.frc2881.karlk.commands.AutoCommands.AbstractAutoCommand;
 import org.usfirst.frc2881.karlk.commands.AutoCommands.Enums.StartingLocation;
+import org.usfirst.frc2881.karlk.commands.AutonomousRobotFinish;
 import org.usfirst.frc2881.karlk.commands.DriveForward;
 import org.usfirst.frc2881.karlk.commands.LiftToHeight;
 import org.usfirst.frc2881.karlk.commands.SetClaw;
 import org.usfirst.frc2881.karlk.commands.TurnToHeading;
+import org.usfirst.frc2881.karlk.subsystems.LiftSubsystem;
 import org.usfirst.frc2881.karlk.subsystems.LiftSubsystem.ClawState;
 
 /**
@@ -18,20 +20,48 @@ public class SwitchSafeSide extends AbstractAutoCommand {
 
     public SwitchSafeSide(StartingLocation start){
 
-        double angle = Math.atan2(33.06, 62.0);
+        double distanceToSwitch = 103.0;
+        double angleRight = Math.atan2(26.0, distanceToSwitch);
+        double angleLeft = Math.atan2(-26.0, distanceToSwitch);
 
-        addSequential(new ConditionalCommand(new TurnToHeading(angle * 180/Math.PI, true), new TurnToHeading(-angle * 180/Math.PI, true)) {
+        addSequential(new ConditionalCommand(
+                new TurnToHeading(angleLeft * 180/Math.PI, true),
+                new TurnToHeading(angleRight * 180/Math.PI, true)) {
+            @Override
+            protected boolean condition() {
+                return start == StartingLocation.LEFT;
+                }
+        });
+
+        addSequential(new ConditionalCommand(
+                new DriveForward((distanceToSwitch/Math.cos(angleLeft)) / 12),
+                new DriveForward((distanceToSwitch/Math.cos(angleRight) + 1) / 12)) {
             @Override
             protected boolean condition() {
                 return start == StartingLocation.LEFT;
             }
         });
 
-        addSequential(new LiftToHeight(3.7 - .17, false));
+        addSequential(new ConditionalCommand(
+                new TurnToHeading(90, false),
+                new TurnToHeading(270, false)) {
+            @Override
+            protected boolean condition() {
+                return start == StartingLocation.LEFT;
+            }
+        });
 
-        addSequential(new DriveForward(62.0 / Math.cos(angle) /12));
+        addSequential(new LiftToHeight(3.7-0.17, false));
+
+        addSequential(new DriveForward(22.0 / 12));
 
         addSequential(new SetClaw(ClawState.OPEN));
+
+        addSequential(new DriveForward( -22.0/12));
+        
+        //addSequential(new AutonomousRobotFinish());
+
+        addSequential(new LiftToHeight(LiftSubsystem.ZERO_ARM_HEIGHT, false));
     }
 
 
